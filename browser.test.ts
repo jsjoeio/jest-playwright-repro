1a8aca419009436d6979bd333700168162131366
Joe Previte <jjprevite@gmail.com>
2021-03-30 15:18:39 -0700

    fix: add code-server test

--- browser.test.ts
+++ browser.test.ts
@@ -3,13 +3,13 @@
 describe("login page", () => {
   beforeEach(async () => {
     await jestPlaywright.resetContext()
-    // await page.goto("http://localhost:8080")
-    await page.goto("https://github1s.com/microsoft/playwright")
+    await page.goto("http://localhost:8080")
+    // await page.goto("https://github1s.com/microsoft/playwright")
   })
 
   it("should see the login page", async () => {
     // It should send us to the login page
-    // expect(await page.title()).toBe("code-server login")
-    expect(await page.title()).toBe("GitHub1s")
+    expect(await page.title()).toBe("code-server login")
+    // expect(await page.title()).toBe("GitHub1s")
   })
 })
