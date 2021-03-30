7678a88a91f64cc756fa137283fc9b3527ceba62
Joe Previte <jjprevite@gmail.com>
2021-03-30 15:59:06 -0700

    feat: add isolated playwright test

---
+++ index.js
@@ -0,0 +1,19 @@
+const { firefox } = require("playwright")
+
+// 1. Install code-server `yarn add global code-server` and run locally: `code-server`
+// 2.
+
+;(async () => {
+  const browser = await firefox.launch()
+  const context = await browser.newContext()
+  const page = await context.newPage()
+
+  await page.goto("http://localhost:8080")
+  const title = await page.title()
+
+  if (title === "code-server login") {
+    console.log("It worked!")
+  } else {
+    console.log("It failed.")
+  }
+})()
