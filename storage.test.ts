f548da563fab8b91e07f069873120a0935b09751
Joe Previte <jjprevite@gmail.com>
2021-04-01 13:50:16 -0700

    feat: add storage test and modify jest config

---
+++ storage.test.ts
@@ -0,0 +1,18 @@
+/// <reference types="jest-playwright-preset" />
+
+describe("storage", () => {
+  beforeEach(async () => {
+    const storageState = JSON.parse(process.env.STORAGE) || {}
+    console.log("Storage ===>", storageState)
+    await jestPlaywright.resetContext({ storageState })
+    await page.goto("http://localhost:8080")
+    // code-server takes a second to load
+    await page.waitForTimeout(1000)
+  })
+
+  it("should keep us logged in using storageState", async () => {
+    // See the editor
+    const codeServerEditor = await page.isVisible(".monaco-workbench")
+    expect(codeServerEditor).toBeTruthy()
+  })
+})
